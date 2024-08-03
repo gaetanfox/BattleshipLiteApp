eeb420468a9a7e856fffd89fbed0cac4ca705f4b
Gaetan Fox <me@gaetanfox.io>
2024-08-03 13:12:12 +0200

    Add project files.

---
+++ BattleshipLite/Program.cs
@@ -0,0 +1,129 @@
+﻿using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel opponent = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                // Display grid from activePlayer on where they fired
+                DisplayShotGrid(activePlayer);
+                // Ask activePlayer for a shot
+                // Determine if valid shot
+                // Determine shot results
+                RecordPlayerShot(activePlayer, opponent);
+
+                // Determine if the game is over
+                // if over, set activePlayer as winner
+                // else, swap positions (activePLayer to opponent)
+
+            } while (winner == null);
+
+            Console.ReadLine();
+        }
+
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            // Asks for a shot (we ask for b2)
+            // Determine whatr row and column that is - split it apart
+            // Determine if that is a valid shot
+            // Go back to the beginning if not a valid shot
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            
+            {
+                if (gridSpot.SpotLetter != currentRow)
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($" {gridSpot.SpotLetter}{gridSpot.SpotNumber} ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Hit)
+                {
+                    Console.Write(" X ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Miss)
+                {
+                    Console.Write(" O ");
+                }
+                else
+                {
+                    Console.Write(" ? ");
+                }
+
+            }
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("Created by Gaetan Fox");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+            Console.WriteLine($"Player information for {playerTitle} ");
+
+            // Ask the user for their name
+            output.UsersName = AskForUsersName();
+            // Load up the shot grid
+            GameLogic.InitializeGrid(output);
+            // Ask the user for their 5 ships placements
+            PlaceShips(output);
+            // Clear the screen
+            Console.Clear();
+
+            return output;
+
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocations.Count + 1}: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again");
+                }
+
+            } while (model.ShipLocations.Count < 5);
+        }
+    }
+}
